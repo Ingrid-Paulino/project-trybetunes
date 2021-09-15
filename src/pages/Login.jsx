2ffaeb7f9de8233cccaeef5b65d7caf6c3b4fb1f
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2021-09-15 18:04:36 -0300

    C2 - Adicionado requisito 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,20 +1,57 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router';
+import { createUser } from '../services/userAPI';
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      nameLogin: '',
+      loading: false,
+      logado: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.criarUsuario = this.criarUsuario.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  async criarUsuario() {
+    const { nameLogin } = this.state;
+    this.setState({ loading: true });
+    await createUser({ name: nameLogin });
+    this.setState({ loading: false, logado: true });
+  }
+
   render() {
+    const { nameLogin, loading, logado } = this.state;
+    const MIN_LENGTH = 3;
+    if (loading) return (<p>Carregando...</p>);
+    if (logado) return (<Redirect to="/search" />);
     return (
       <div data-testid="page-login">
         <form>
           <label htmlFor="login-name">
+            Nome:
             <input
               type="text"
               id="login-name"
-              name="login-name"
+              name="nameLogin"
+              value={ nameLogin }
+              onChange={ this.handleChange }
               data-testid="login-name-input"
             />
           </label>
           <button
-            type="submit"
+            type="button"
+            disabled={ nameLogin.length < MIN_LENGTH }
+            onClick={ () => this.criarUsuario() }
             data-testid="login-submit-button"
           >
             Entrar
